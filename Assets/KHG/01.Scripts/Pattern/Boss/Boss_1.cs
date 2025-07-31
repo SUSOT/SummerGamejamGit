fa69593ec10999636a6469533f30a370d670b34b
고한결 <gyeol0411@naver.com>
2025-07-31 22:47:39 +0900

    .add Boss

---
+++ Assets/KHG/01.Scripts/Pattern/Boss/Boss_1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Boss_1 : TimeLinePattern
+{
+    public Boss_1(float startTime) : base(startTime)
+    {
+    }
+
+    public override void Execute()
+    {
+    }
+}
