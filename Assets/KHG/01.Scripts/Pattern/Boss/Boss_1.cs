d5077778f70a818d9d02753264b3a29fef43b3d1
고한결 <gyeol0411@naver.com>
2025-08-01 00:03:03 +0900

    .update Boss1

--- Assets/KHG/01.Scripts/Pattern/Boss/Boss_1.cs
+++ Assets/KHG/01.Scripts/Pattern/Boss/Boss_1.cs
@@ -1,12 +1,22 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Boss_1 : TimeLinePattern
 {
+    [SerializeField] private Animator _bossAnimator;
     public Boss_1(float startTime) : base(startTime)
     {
     }
 
     public override void Execute()
     {
+
+    }
+    private void BossSequence()
+    {
+        Sequence mySequence = DOTween.Sequence();
+
+        mySequence.AppendCallback(()=> _bossAnimator.Play("B1_P1"));
+        mySequence.AppendInterval(15);
     }
 }
