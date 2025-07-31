9b717e84b0415956e5cafe355290000376af2d8e
dlckdgh0414 <ci5976547@gmail.com>
2025-07-31 23:54:08 +0900

    feat : Pattern2

---
+++ Assets/LCH/01.Scripts/Pattern/Pattern2.cs
@@ -0,0 +1,26 @@
+using GondrLib.Dependencies;
+using GondrLib.ObjectPool.Runtime;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern2 : TimeLinePattern
+{
+
+    [SerializeField] private PoolingItemSO boomItem;
+    [SerializeField] private int SpawnCount;
+    [SerializeField] private List<Vector2> spawnPoints;
+    [Inject] private PoolManagerMono _poolManager;
+    public Pattern2(float startTime) : base(startTime)
+    {
+    }
+
+    private void OnEnable()
+    {
+        Injector.Instance.InjectRuntime(this);
+    }
+
+    public override void Execute()
+    {
+        
+    }
+}
