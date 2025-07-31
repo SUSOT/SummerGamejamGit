f13fc4e00dfe0b932e6b081be23a5649b8d1d117
SUSOT <a01076754042@gmail.com>
2025-07-31 23:35:09 +0900

    Feat : LCM_Pattern1

--- Assets/LCM/01.Scripts/Timeline/Pattern1.cs
+++ Assets/LCM/01.Scripts/Timeline/Pattern1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using GondrLib.Dependencies;
 using GondrLib.ObjectPool.Runtime;
+using KHG.Bullets;
 using LCM._01.Scripts.Bullets;
 using UnityEngine;
 
@@ -11,6 +12,7 @@ namespace LCM._01.Scripts.Timeline
         [SerializeField] private PoolingItemSO triangleCannon;
         [SerializeField] private PoolingItemSO wall;
         [SerializeField] private PoolingItemSO crossLaser;
+        [SerializeField] private PoolingItemSO laser;
         [Inject] private PoolManagerMono _poolManager;
         
         public Pattern1(float startTime) : base(startTime)
@@ -33,10 +35,37 @@ namespace LCM._01.Scripts.Timeline
             for (int i = -1; i < 2; ++i)
             {
                 CrossLaserBullet cr = _poolManager.Pop<CrossLaserBullet>(crossLaser);
-                cr.transform.position = new Vector3(i * 4, 0, 0);
+                cr.transform.position = new Vector3(i * 20, 0, 0);
+                yield return new WaitForSeconds(1f);
+            }
+            yield return new WaitForSeconds(3f);
+            TriangleCannon tc = _poolManager.Pop<TriangleCannon>(triangleCannon);
+            tc.transform.position = new Vector3(30, 30, 0);
+            tc.MovePosition = Vector2.zero;
+            tc.FireDuration = 0.2f;
+            for (int i = 0; i < 10; ++i)
+            {
+                WallBullet wb = _poolManager.Pop<WallBullet>(wall);
+                int random = Random.Range(0, 2);
+                wb.transform.position = random == 1 ? new Vector3(30f, 0, 0) : new Vector3(-30f, 0, 0);
+                wb.MoveDirection = random == 1 ? Vector2.left : Vector2.right;
+                wb.MoveSpeed = 8f;
+                if (i > 4)
+                {
+                    LaserBullet lb = _poolManager.Pop<LaserBullet>(laser);
+                    lb.transform.position = Vector2.zero;
+                    lb.transform.rotation = Quaternion.Euler(0,0,Random.Range(0,360));
+                }
+                yield return new WaitForSeconds(Random.Range(2f, 3.5f));
             }
 
-            yield return new WaitForSeconds(7f);
+            for (int i = 0; i < 10; ++i)
+            {
+                LaserBullet lb = _poolManager.Pop<LaserBullet>(laser);
+                lb.transform.position = Vector2.zero;
+                lb.transform.rotation = Quaternion.Euler(0,0,Random.Range(0,360));
+                yield return new WaitForSeconds(0.1f);
+            }
         }
     }
 }
