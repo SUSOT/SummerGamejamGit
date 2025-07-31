6d76dde324904ae4d3959f76c1b12a41eb3c220c
ruddnjs13 <ruddnjs013@gmail.com>
2025-08-01 00:13:29 +0900

    fix : pattern

--- Assets/LKW/01.Scripts/TimeLine/TimeLinePattern.cs
+++ Assets/LKW/01.Scripts/TimeLine/TimeLinePattern.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public abstract class TimeLinePattern : MonoBehaviour
 {
-    public float startTime;
     
     public abstract void Execute();
-
-    public TimeLinePattern(float startTime)
-    {
-        this.startTime = startTime;
-    }
 }
