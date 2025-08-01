598519aade3360f11a88a4d6bc69c68017dcb32a
고한결 <gyeol0411@naver.com>
2025-08-01 17:08:20 +0900

    .update Boss2

--- Assets/KHG/01.Scripts/TestLaserGenegenerator.cs
+++ Assets/KHG/01.Scripts/TestLaserGenegenerator.cs
@@ -1,6 +1,7 @@
 using GondrLib.Dependencies;
 using GondrLib.ObjectPool.Runtime;
 using KHG.Bullets;
+using KHG.Obstacles;
 using System.Collections;
 using UnityEngine;
 
@@ -19,14 +20,14 @@ public class TestLaserGenegenerator : MonoBehaviour
     }
     private IEnumerator ShootingBullet()
     {
-        CircleWorm circleWOrm = _poolManager.Pop<CircleWorm>(worm);
+        WallGen circleWOrm = _poolManager.Pop<WallGen>(worm);
         if (circleWOrm == null)
         {
             Debug.LogError("풀에서 ExplodeBullet을 가져오지 못했습니다.");
             yield break;
         }
 
-        circleWOrm.rotationSpeed = 45f;
+        circleWOrm.SpawnVector = transform.position + new Vector3(Random.Range(-17f, 17f), Random.Range(-10f, 10f));
 
         yield return new WaitForSeconds(0.5f);
         StartCoroutine(ShootingBullet());
