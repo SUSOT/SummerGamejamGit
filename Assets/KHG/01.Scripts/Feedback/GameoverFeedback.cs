c4cd6035bbe284cb151300e375cc3c03fdd481c8
고한결 <gyeol0411@naver.com>
2025-08-02 00:13:41 +0900

    .update Gameover

--- Assets/KHG/01.Scripts/Feedback/GameoverFeedback.cs
+++ Assets/KHG/01.Scripts/Feedback/GameoverFeedback.cs
@@ -1,12 +1,26 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+[RequireComponent((typeof(Volume)))]
 public class GameoverFeedback : Feedback
 {
-    [SerializeField] private Volume GameoverVolume;
+    [SerializeField] private GameEventChannelSO cameraChannel;
+    [SerializeField] private Transform plr;
+
+    private Volume GameoverVolume;
+
+    private void Awake()
+    {
+        GameoverVolume = GetComponent<Volume>();
+    }
     public override void CreateFeedback()
     {
         GameoverVolume.enabled = true;
+
+        if (cameraChannel == null) return;
+        cameraChannel.RaiseEvent(CameraEvent.CameraFocusEvent);
+        Time.timeScale = 0;
     }
 
     public override void StopFeedback()
