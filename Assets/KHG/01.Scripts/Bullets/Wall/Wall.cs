a419ba5b62d8ffa89d38fe9001b7edc4ca4a32ea
고한결 <gyeol0411@naver.com>
2025-08-01 13:18:40 +0900

    .update Wall

--- Assets/KHG/01.Scripts/Bullets/Wall/Wall.cs
+++ Assets/KHG/01.Scripts/Bullets/Wall/Wall.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using GondrLib.ObjectPool.Runtime;
 using LCM._01.Scripts;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -11,13 +12,23 @@ namespace KHG.Obstacles
         [SerializeField] private GameObject wallObj;
         [SerializeField] private SpriteRenderer _warnRenderer;
 
+        public Vector3 SpawnVector { get; set; }
+
         public UnityEvent OnWallDeployed;
         public float WarnTime = 1.5f;
 
         private Pool _wallPool;
 
-        private void Start()
+        protected override void OnEnable()
         {
+            base.OnEnable();
+            StartCoroutine(StartCo());
+        }
+
+        private IEnumerator StartCo()
+        {
+            transform.position = SpawnVector;
+            yield return new WaitForSeconds(0.1f);
             wallObj.SetActive(false);
             _warnRenderer.gameObject.SetActive(false);
             SetWall();
@@ -58,7 +69,8 @@ namespace KHG.Obstacles
 
         public override void ResetItem()
         {
-            throw new System.NotImplementedException();
+            wallObj.SetActive(false);
+            _warnRenderer.gameObject.SetActive(false);
         }
     }
 }
